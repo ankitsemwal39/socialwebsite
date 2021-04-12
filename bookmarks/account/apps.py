959759e96fc45cd45c43c7286061baccfe58a273
ankit semwal <ankitsemwal39@gmail.com>
2021-04-12 12:31:59 +0530

    initial commit

---
+++ bookmarks/account/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class AccountConfig(AppConfig):
+    name = 'account'
+    # def read(self):
+    #     # noinspection PyUnresolvedReferences
+    #     import users.signals
